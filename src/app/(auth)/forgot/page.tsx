ab15f4be4f1df5ddf55b38abf754aa9cfc8530c6
Tomáš Kotous <kotous@rtsoft.cz>
2025-01-13 22:03:53 +0100

    Přidání šablony stránky pro zapomenuté heslo a registrace

---
+++ src/app/(auth)/forgot/page.tsx
@@ -0,0 +1,5 @@
+'use client';
+
+export default function Forgot() {
+    return <div>Forgot password?</div>;
+}
--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,13 +1,14 @@
 'use client';
 
 import styles from './page.module.scss';
-import { Button } from '@/app/components/control/Button';
+import { Button } from '@/app/components/button/Button';
 import { Input } from '@/app/components/input/Input';
 import { SocialButtonsDivider } from './components/SocialButtonsDivider';
 import { Facebook } from '@/app/components/icons/Facebook';
 import { Google } from '@/app/components/icons/Google';
 import { PasswordInput } from '@/app/components/input/password/PasswordInput';
-import { ControlStyleType } from '@/app/components/control/types';
+import { ButtonStyleType } from '@/app/components/button/types';
+import Link from 'next/link';
 
 export default function SignIn() {
     return (
@@ -15,26 +16,24 @@ export default function SignIn() {
             <div className={styles.signinBox__header}>
                 <h1>Log in</h1>
                 <p>
-                    Don&apos;t you have an account? <a href="/signup">Sign up</a>
+                    Don&apos;t you have an account? <Link href="/signup">Sign up</Link>
                 </p>
             </div>
 
             <div className={styles.signinForm}>
                 <Input type="email" id="email" label="Your email" required />
                 <PasswordInput required />
-                <a href="/forgot">Forgot your password</a>
-                <Button styleType={ControlStyleType.PRIMARY} type="submit">
-                    Log in
-                </Button>
+                <Link href="/forgot">Forgot your password</Link>
+                <Button type="submit">Log in</Button>
             </div>
 
             <SocialButtonsDivider />
 
             <div className={styles.socialButtonsWrapper}>
-                <Button styleType={ControlStyleType.OUTLINED} icon={<Facebook />}>
+                <Button styleType={ButtonStyleType.OUTLINED} icon={<Facebook />}>
                     Log in with Facebook
                 </Button>
-                <Button styleType={ControlStyleType.OUTLINED} icon={<Google />}>
+                <Button styleType={ButtonStyleType.OUTLINED} icon={<Google />}>
                     Log in with Google
                 </Button>
             </div>
